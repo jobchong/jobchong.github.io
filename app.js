8473bbb259856817b33f897a2243de37cd3a09d4
jobchong <jobchong@gmail.com>
2016-06-19 15:05:28 +0800

    Node is server side guys

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var companiesHouse = require('companies-house')('dhokmSz0Kxz0LOMVRmhAo4x1Fxlcdh0oAGBxnUSd')
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -25,6 +27,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+
+companiesHouse.search('certsimple', function(err, res){
+	console.log(err, res)
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

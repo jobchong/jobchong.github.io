e7ca39b511eaf4fff998ae6744bab4a50926c1e3
jobchong <jobchong@gmail.com>
2016-07-05 22:13:36 +0800

    test passing of variables to client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var alertvar = "I have been passed!"
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { myVar: alertvar });
 });
 
 module.exports = router;

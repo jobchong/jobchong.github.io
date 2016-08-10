36e7c5ecb890208f7769187ea9141288b18fb76b
jobchong <jobchong@gmail.com>
2016-08-10 21:08:07 +0800

    forgot definition

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
-
+var ObjectID = mongodb.ObjectID;
 var uen = "uen";
 var db;
 

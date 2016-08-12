a1678a86745458ca49d1d3fe4b7e1efb561d9dad
jobchong <jobchong@gmail.com>
2016-08-12 16:59:40 +0800

    add test

--- app.js
+++ app.js
@@ -52,6 +52,11 @@ router.use(function(req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+
 router.route("/uen/:id")
     .get(function(req, res) {
 	db.collection(uen).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
